7b2ac7c618ae69f5d99336ff255e117dfbd94e35
ilias-chat <ilias.chat.7@gmail.com>
2024-10-29 00:35:23 -0700

    test cookies

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI, status
+from datetime import timedelta
+from typing import Annotated
+from fastapi import Cookie, Depends, FastAPI, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.exc import IntegrityError
+from sqlmodel import Session
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.requests import Request
 from starlette.responses import FileResponse
+
+from app.core.config import TokenSettings
+from app.db.dependencies import get_session
+from app.models.users import Tokens, UserLogin
 
 from .errors.item_not_found import ItemNotFound
 from .errors.login_credentials_invalid import LoginCredentialsInvalid
@@ -18,6 +26,60 @@ app = FastAPI(
     docs_url=f"/api/v1/docs",
     redoc_url=None
 )
+
+@app.post("/api/v1/web/login", response_model=Tokens)
+async def login(
+    *,
+    session: Session = Depends(get_session),
+    req: Request,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    credentials = UserLogin(email=form_data.username, password=form_data.password)
+    user = await auth.authenticate_user(session, credentials, req)
+    if not user:
+        raise LoginCredentialsInvalid()
+    
+    # Generate the access and refresh tokens
+    tokens = auth.create_tokens(user)
+    
+    response = JSONResponse(content={
+        "access_token": tokens.access_token,
+        "id":tokens.id,
+        "email":tokens.email,
+        "first_name":tokens.first_name,
+        "last_name":tokens.last_name,
+        "profile":tokens.profile,
+        "profile_id":tokens.profile_id,
+        "image_path": tokens.image_path,
+        "image_id":tokens.image_id
+    })
+    response.set_cookie(
+        key="refresh_token",
+        value=tokens.refresh_token,
+        httponly=True,
+        max_age=int(timedelta(minutes=15).total_seconds()),  
+        secure=True,  
+        samesite="strict"
+    )
+    return response
+
+
+@app.post("/api/v1/web/refresh_token")
+async def refresh_token(
+    *, 
+    refresh_token: Annotated[str | None, Cookie()] = None  # Get the refresh token from the HTTP-only cookie
+):
+    # Check if the refresh token is valid
+    if refresh_token is None:
+        raise HTTPException(status_code=401, detail="Refresh token is missing")
+
+    # Optionally validate the refresh token here
+    # If validation passes, create new tokens
+    token_data = {"id": 2, "profile_id": 2}
+    access_token=auth.create_token(token_data, timedelta(seconds=10)),
+
+    return JSONResponse(content={"access_token": access_token})
+
 
 @app.exception_handler(IntegrityError)
 async def integrity_error_handler(_, exc: IntegrityError):
@@ -58,7 +120,10 @@ async def login_credentials_invalid_handler(req: Request, exc: LoginCredentialsI
     return JSONResponse(status_code=exc.status_code, content={"error_detail": exc.message})
 
 
-origins = ["*"]
+origins = [
+    "http://localhost:4200",
+    "http://localhost"
+    ]
 
 app.add_middleware(
     CORSMiddleware,
